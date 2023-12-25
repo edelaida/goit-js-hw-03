3870c22b89e3d547573376b6ad79390d51e08ddc
Levina Tani <edelaida@gmail.com>
2023-12-25 13:27:30 +0200

    newArray

--- js/task-2.js
+++ js/task-2.js
@@ -11,12 +11,6 @@
 // <country> — це країни доставки
 // <totalPrice> — це загальна вартість замовлення, що включає вартість
 // товару і його доставки
-// Візьми код нижче і встав після оголошення своєї функції
-// для перевірки коректності її роботи. У консоль будуть виведені результати її роботи.
-
-// console.log(getShippingMessage("Australia", 120, 50));
-// console.log(getShippingMessage("Germany", 80, 20));
-// console.log(getShippingMessage("Sweden", 100, 20));
 
 // function getShippingMessage(country, price, deliveryFee) {
 //     const totalPrice = (price + deliveryFee);
@@ -27,35 +21,62 @@
 // console.log(getShippingMessage("Sweden", 100, 20));
 
 
-// Задача 2. Форматування повідомлення
+// ********************* Задача 2. Форматування повідомлення **********
 
 // Оголоси функцію formatMessage(message, maxLength), яка приймає рядок(параметр message)
 // та перевіряє його довжину відповідно до заданої максимальної довжини(параметр maxLength).
 
 // Доповни код функції таким чином, що:
-
-// Якщо довжина рядка дорівнює або менша за maxLength, то функція повертає початковий 
+// Якщо довжина рядка дорівнює або менша за maxLength, то функція повертає початковий
 // рядок без змін.
 // Якщо довжина перевищує maxLength, то функція обрізає рядок до maxLength символів,
 //     додає трикрапку "..." в кінці та повертає обрізану версію.
-// Візьми код нижче і встав після оголошення своєї функції для перевірки коректності 
-// її роботи.У консоль будуть виведені результати її роботи.
 
-function formatMessage(message, maxLength) {
-    if (message.length <= maxLength) {
-        return (message);
-    } else {
-        message = message.slice(0, maxLength);
-        return (`${message}...`);
-    } 
-  }     
+// function formatMessage(message, maxLength) {
+//     if (message.length <= maxLength) {
+//         return (message);
+//     } else {
+//         message = message.slice(0, maxLength);
+//         return (`${message}...`);
+//     }
+//   }
     
-console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
-console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
-console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
-console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
-console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
-console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
+// console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
+// console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
+// console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
+// console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
+// console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
+// console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
+
+// ****************** 2  ****************
+// Напиши функцію під назвою makeArray, яка приймає три параметри: firstArray(масив),
+//     secondArray(масив) і maxLength(число).Функція повинна створювати новий масив,
+//         який містить усі елементи з firstArray, а потім усі елементи з secondArray.
+
+// Якщо кількість елементів у новому масиві перевищує maxLength, функція повинна повернути
+//  копію масиву з довжиною maxLength елементів.
+// В іншому випадку функція повинна повернути весь новий масив.
+
+function makeArray(firstArray, secondArray, maxLength) {
+    const newArray = [];
+    let num = firstArray.length;
+    for (let i = 0; i < maxLength; i++) {
+       if (i < firstArray.length) {
+           newArray.push(firstArray[i]);
+       } else {
+           let j = i - num;
+           newArray.push(secondArray[j]);
+       }     
+   }
+    return newArray;
+}
+
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
 
 
  
--- js/task-3.js
+++ js/task-3.js
@@ -9,13 +9,6 @@
 // При розрахунку загальної ширини орієнтуйся на те, що значення box - sizing
 // дорівнює border - box.
 
-// Візьми код нижче і встав після оголошення своєї функції для перевірки
-// коректності її роботи.У консоль будуть виведені результати її роботи.
-
-// console.log(getElementWidth("50px", "8px", "4px")); // 74
-// console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-// console.log(getElementWidth("200px", "0px", "0px")); // 200
-
 // function getElementWidth(content, padding, border) {
 //     return (Number.parseFloat(content) + 2 * Number.parseFloat(padding) + 2 * Number.parseFloat(border));
 // }
@@ -24,28 +17,60 @@
 // console.log(getElementWidth("200px", "0px", "0px"));
 
 
-// Задача 3. Перевірка спаму
+// ***************  Задача 3. Перевірка спаму  ***********
+
 // Функція checkForSpam(message) приймає рядок(параметр message),
 //     перевіряє його на вміст заборонених слів spam і sale,
-//     і повертає результат перевірки.Слова в рядку параметра message 
+//     і повертає результат перевірки.Слова в рядку параметра message
 //         можуть бути в довільному регістрі, наприклад SPAM або sAlE.
 
 // Доповни код функції таким чином, що:
-
 // Якщо знайдено заборонене слово (spam або sale), то функція повертає буль true
 // Якщо в рядку відсутні заборонені слова, функція повертає буль false
-// Візьми код нижче і встав після оголошення своєї функції для перевірки 
-// коректності її роботи.У консоль будуть виведені результати її роботи.
+   
+// function checkForSpam(message) {
+//     message = message.toLowerCase()
+//     return (message.includes('spam') || message.includes('sale'));
+//     }
     
-function checkForSpam(message) {
-    message = message.toLowerCase()
-    return (message.includes('spam') || message.includes('sale')); 
-    } 
-    
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+// console.log(checkForSpam("Latest technology news")); // false
+// console.log(checkForSpam("JavaScript weekly newsletter")); // false
+// console.log(checkForSpam("Get best sale offers now!")); // true
+// console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+// console.log(checkForSpam("Trust me, this is not a spam message")); // true
+// console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+// console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+
+// *********************  3************
+
+// Напиши функцію filterArray(numbers, value), яка приймає масив
+// чисел(numbers) та значення(value) як параметри.
+// Функція повинна повертати новий масив лише тих чисел із
+//  масиву numbers, які більші за значення value.
+
+// Усередині функції:
+
+// Створи порожній масив, у який будеш додавати підходящі числа.
+// Використай цикл для ітерації кожного елемента масиву numbers.
+// Використовуй умовний оператор if усередині циклу для перевірки
+// кожного елемента и додавання до свого масиву.
+// Поверни свій новий масив з підходящими числами як результат.
+// Візьми код нижче і встав після оголошення своєї функції для
+// перевірки коректності її роботи.У консоль будуть виведені
+// результати її роботи.
+
+function filterArray(numbers, value) {
+    const newArray = [];
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] > value) {
+            newArray.push(numbers[i]);
+        }        
+    }
+    return newArray;
+}
+
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
